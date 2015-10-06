7cc00f2a91744ad4011437c4601f6a3442d5902b
midori1 <tanghongyin@foxmail.com>
2015-10-06 22:11:47 +0800

    change url

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('miniblog.urls', namespace="miniblog")),
+    url(r'^', include('miniblog.urls', namespace="miniblog")),
 )
